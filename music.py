11e5c4e44e3923773b4bc4bfeb7b0cc9f4e979d2
ca5500 <cassidyw11@nycstudents.net>
2024-11-19 09:26:19 -0500

    .

---
+++ music.py
@@ -0,0 +1,15 @@
+json_string = {[
+    "id": 2
+    "title": "its beginning to look a lot like christmas"
+],
+    "lyrics" : "Everywhere you go, Take a look at the five and ten"
+"it's glistening once again"
+"With candy canes and silver lanes that glow"
+}
+
+
+songs = [
+    {"file": "song1.mp3", "title": "Last Christmas"},
+    {"file": "song2.mp3", "title": "It's Beginning to Look a Lot "},
+    {"file": "song3.mp3", "title": "Song 3"}
+]
